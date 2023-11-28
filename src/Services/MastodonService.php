b0da68d95b0a977b65e10170514e894cec88100d
Nic Barr <1134853+theothernic@users.noreply.github.com>
2023-11-27 23:03:12 -0600

    scaffolding client and configuration

--- src/Services/MastodonService.php
+++ src/Services/MastodonService.php
@@ -2,10 +2,31 @@
     namespace Bearlovescode\Mastodon\Services;
 
     use Bearlovescode\Mastodon\Clients\ApiClient;
+    use Bearlovescode\Mastodon\Models\Configuration;
+    use Bearlovescode\Mastodon\Models\Toot;
 
     class MastodonService
     {
-        public function __construct(Configuration $config)
+        private ApiClient $client;
+
+        public function __construct(private Configuration $config)
         {
+            $this->configureClient();
+        }
+
+        public function publish (Toot $toot) : void
+        {
+
+        }
+
+        private function configureClient() : void
+        {
+            if (empty($this->client))
+            {
+                $this->client = new ApiClient([
+                    'base_uri' => (string) $this->config->instance
+                ]);
+            }
+
         }
     }
