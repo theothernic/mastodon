a47434033f34f061b7320b59d0f8900ca2bdc1c1
Nic Barr <1134853+theothernic@users.noreply.github.com>
2023-11-27 22:39:43 -0600

    scaffolding client and configuration

--- src/Clients/ApiClient.php
+++ src/Clients/ApiClient.php
@@ -9,4 +9,9 @@
         {
 
         }
+
+        public function authenticate()
+        {
+
+        }
     }
--- src/Models/Configuration.php
+++ src/Models/Configuration.php
@@ -1,11 +1,44 @@
 <?php
     namespace Bearlovescode\Mastodon\Models;
 
+    use League\Uri\Uri;
     use Bearlovescode\Datamodels\DataModel;
+
 
     class Configuration extends DataModel
     {
         public Uri $instance;
+        public string $clientId;
+        public string $clientSecret;
 
+        public function __construct(array|object $data = null)
+        {
+            $this->setInstanceUrlFromData($data);
+
+            parent::__construct($data);
+        }
+
+
+
+        private function setInstanceUrlFromData(array|object &$data) : void
+        {
+            if (gettype($data) === 'array')
+            {
+                $this->setInstanceUrl($data['instance']);
+                unset($data['instance']);
+            }
+
+            elseif (gettype($data) === 'object')
+            {
+                $this->setInstanceUrl($data->instance);
+                unset($data->instance);
+            }
+        }
+
+
+        public function setInstanceUrl(string $url) : void
+        {
+            $this->instance = Uri::new($url);
+        }
 
     }
